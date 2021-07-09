59113af9cc9ccd3f93925e846afc2981b11636c7
Nia K <nsjkappen@gmail.com>
2021-07-09 15:56:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Follow along with the examples here
+
+
+function sayHello() {
+
+    console.log('Hello');
+}
+
+sayHello();
+
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  } 
+
+  sayHelloToIsabel();
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+
+  sayHelloToSofia();
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  } 
+
+  sayHelloToBrendan();
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log('I was called!');
+  } 
+
+  console.log(say("Howdy", "partner")); 
